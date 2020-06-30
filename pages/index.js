f43afbc0ef98e6496906cd1b3edc845095c1fa76
allenhj <allendotjohnson@gmail.com>
2020-06-30 12:39:03 -0400

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,14 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Simple Run Distance Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;500;900&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#00c444" />
       </Head>
 
       <main>
@@ -229,6 +235,7 @@ export default function Home() {
           .flex {
             flex-direction: column;
             align-items: flex-start;
+            width: 100%;
           }
 
           .flex > * {
